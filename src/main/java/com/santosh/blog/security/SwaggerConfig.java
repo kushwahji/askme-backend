d40129a3ea0d6da67a74c5bed2ea049b123dd77a
kushwahji <santoshkumar021990@gmail.com>
2022-07-29 17:13:03 +0530

    change properties

--- src/main/java/com/santosh/blog/security/SwaggerConfig.java
+++ src/main/java/com/santosh/blog/security/SwaggerConfig.java
@@ -1,44 +1,42 @@
 /**
  * 
  */
-
-package com.santosh.blog.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-/**
- * @author RITIK
- *
- */
-@Configuration
-
-@EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
-	}
-
-	private ApiInfo apiInfo() {
-		ApiInfo api = new ApiInfoBuilder().title("").build();
-		return api;
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
-}
+/*
+ * 
+ * package com.santosh.blog.security;
+ * 
+ * import org.springframework.context.annotation.Bean; import
+ * org.springframework.context.annotation.Configuration; import
+ * org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+ * import
+ * org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+ * import springfox.documentation.builders.ApiInfoBuilder; import
+ * springfox.documentation.builders.PathSelectors; import
+ * springfox.documentation.builders.RequestHandlerSelectors; import
+ * springfox.documentation.service.ApiInfo; import
+ * springfox.documentation.spi.DocumentationType; import
+ * springfox.documentation.spring.web.plugins.Docket; import
+ * springfox.documentation.swagger2.annotations.EnableSwagger2;
+ * 
+ *//**
+	 * @author RITIK
+	 *
+	 *//*
+		 * @Configuration
+		 * 
+		 * @EnableSwagger2 public class SwaggerConfig extends WebMvcConfigurationSupport
+		 * {
+		 * 
+		 * @Bean public Docket api() { return new
+		 * Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any
+		 * ()) .paths(PathSelectors.any()).build().apiInfo(apiInfo()); }
+		 * 
+		 * private ApiInfo apiInfo() { ApiInfo api = new
+		 * ApiInfoBuilder().title("").build(); return api; }
+		 * 
+		 * @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		 * registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+		 * "classpath:/META-INF/resources/");
+		 * registry.addResourceHandler("/webjars/**").addResourceLocations(
+		 * "classpath:/META-INF/resources/webjars/"); } }
+		 */
